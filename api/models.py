c79ca95953c6c23530bd64ac2d5e48b231bf6e4c
Dmitrii Rumyancev <rychanya@gmail.com>
2020-08-22 11:21:50 +0300

    add custom user model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,9 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
+
+
+class User(AbstractUser):
+    pass
 
 
 class Shop(models.Model):
